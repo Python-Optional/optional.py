e183818998fbdafd306be33f9473fe882020c3f2
cbefus <cbefus@indeed.com>
2018-09-07 17:48:42 -0500

    Removed the __neq__ function.

--- optional.py
+++ optional.py
@@ -28,9 +28,6 @@ class Optional:
 
         return other.get() == self.get()
 
-    def __neq__(self, other):
-        return not self == other
-
     def is_present(self):
         self._presence_checked = True
         return self._thing is not None
