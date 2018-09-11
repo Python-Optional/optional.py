0c632534d0501bb575f072cfeb7972d3ff4e71c8
cbefus <cbefus@indeed.com>
2018-09-10 19:29:44 -0500

    Updating setup.py with more info.

--- setup.py
+++ setup.py
@@ -4,18 +4,29 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='optional.py',
-    version='0.1.0',
+    version='0.1.1',
     description='An implementation of the Optional object in Python',
     long_description=readme,
+    long_description_content_type='txt/markdown',
     author='Chad Befus',
     author_email='crbefus@gmail.com',
     url='https://github.com/cbefus/optional.py',
-    download_url = 'https://github.com/cbefus/optional.py/archive/v0.1.0.tar.gz',
-    license=license,
-    packages=find_packages(exclude=('test'))
+    download_url='https://github.com/cbefus/optional.py/archive/v0.1.0.tar.gz',
+    license='GNU General Public License (GPL)',
+    packages=find_packages(exclude=('test')),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries'
+    ],
+    keywords='optional datatype library',
+    python_requires='>=2.0'
 )
