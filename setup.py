1b268a3ea0bb57db7881c4908fb04b02084605d1
cbefus <crbefus@gmail.com>
2018-09-12 17:41:07 -0500

    Pushing major version because license change is breaking.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='optional.py',
-    version='0.1.4',
+    version='0.2.0',
     description='An implementation of the Optional object in Python',
     long_description=readme,
     long_description_content_type='text/markdown',
