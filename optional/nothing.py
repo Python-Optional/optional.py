3deb0274bb77e5a1e1b8a8221138229ae98d7c3b
Derek Passen <dpassen1@gmail.com>
2025-10-10 18:23:53 -0500

    Fix override types

--- optional/nothing.py
+++ optional/nothing.py
@@ -1,18 +1,23 @@
 from types import NotImplementedType
-from typing import Any
+from typing import final
+
+from typing_extensions import override
 
 
+@final
 class Nothing:
     """Represents the absence of a value.
 
     Rarely instantiated on its own, see :func:`Optional.empty`"""
 
-    def __eq__(self, other: Any) -> bool | NotImplementedType:
+    @override
+    def __eq__(self, other: object) -> bool | NotImplementedType:
         if not isinstance(other, Nothing):
             return NotImplemented
 
         return True
 
+    @override
     def __repr__(self) -> str:
         return "Optional.empty()"
 
